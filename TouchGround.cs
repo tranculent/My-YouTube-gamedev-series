afdc9917f1e9656015f834156598e8f0f26b8ec4
Yanko Mirov <45804656+tranculent@users.noreply.github.com>
2019-08-26 14:16:16 +0100

    Add files via upload
    File for ToString and Random video on Unity

---
+++ TouchGround.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+/* 
+* Randomize gravity value
+* Change text content in our TexMeshPro object
+*/
+
+public class TouchGround : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI txt1;
+    
+    float randomGravityValue = 0;
+
+    private void Start()
+    {
+        randomGravityValue = Random.Range(0.1f, 1.5f);
+        GetComponent<Rigidbody2D>().gravityScale = randomGravityValue;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // randomGravityValue = 1.5f;
+        // randomGravityValue.ToString() = "1.5f"
+        txt1.text = randomGravityValue.ToString();
+    }
+}
