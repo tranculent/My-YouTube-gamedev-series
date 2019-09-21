59c40c8a88980ffbe647cce0126627094a9d109c
Yanko Mirov <45804656+tranculent@users.noreply.github.com>
2019-09-21 13:09:31 +0100

    Add files via upload
    Make the bullet going towards mouse position in world space.

---
+++ BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    private Rigidbody2D rb2;
+
+    Vector2 mousePos;
+
+    private void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 directionToShoot = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        rb2.velocity = directionToShoot * speed;
+    }
+
+    private void Update()
+    {
+        Debug.Log(transform.position.x);
+    }
+}
